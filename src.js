e6e69ca2549ee286f160005b1cd538e58bb194b7
lb123658 <79328251+LB123658@users.noreply.github.com>
2022-10-14 21:40:10 -0500

    Create src.js

---
+++ src.js
@@ -0,0 +1,8 @@
+var input = document.getElementById("input");
+if (window.location.search.split("=").length != 0) {
+  input.value = decodeURIComponent(location.search.split("=")[1].replace(/\+/gi, " "));
+  input.click();
+  input.focus();
+} else {
+  input.value = "";
+}
