af74c830f0c939c6102abc77b98aa55a366432ab
lb123658 <79328251+LB123658@users.noreply.github.com>
2022-10-15 10:17:49 -0500

    Update src.js

--- src.js
+++ src.js
@@ -3,6 +3,7 @@ if (window.location.search.split("=").length != 0) {
   input.value = decodeURIComponent(location.search.split("=")[1].replace(/\+/gi, " "));
   input.click();
   input.focus();
+  input.select();
 } else {
   input.value = "";
 }
